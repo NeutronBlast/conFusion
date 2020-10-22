86178f49e1e82e2b3b687e738bdae5dd1919e96d
Frank Hesse <hyperschnell11@outlook.sk>
2020-10-22 02:17:58 -0400

    router link passing parameters in URL

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
-
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,12 +10,20 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
+  dish: Dish;
 
-  @Input() dish: Dish;
-
-  constructor() { }
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute, 
+    private location: Location) { }
 
   ngOnInit(): void {
+    /* Fetches the ID from the parameter */
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void{
+    this.location.back();
   }
 
 }
