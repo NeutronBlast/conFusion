bc5deb87e669d9643de25c50198a55c7298853bd
Frank Hesse <hyperschnell11@outlook.sk>
2020-10-25 11:07:44 -0400

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 /* Components */
 import { MenuComponent } from './menu/menu.component';
@@ -63,7 +66,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
   ],
   providers: [
     DishService,
